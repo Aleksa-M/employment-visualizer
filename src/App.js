721b2243d9aebeb3e0d6011d9166402918d16a37
a2misic <a2misic@uwaterloo.ca>
2025-08-14 10:57:01 -0400

    renamed routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { About } from './pages/about';
 import { Indigenous } from './pages/indigenous';
-import { AnalysisIndex } from './pages/AnalysisIndex'
-import { PrimerTemp } from './pages/PrimerTemp'
-import { Map } from './pages/Map'
+import { AnalysisIndex } from './pages/AnalysisIndex';
+import { PrimerTemp } from './pages/PrimerTemp';
 import CanadaMap from './pages/CanadaMap';
 
 // app
@@ -14,9 +13,9 @@ function App() {
       <Route index element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/indigenous" element={<Indigenous />} />
-      <Route path="/AnalysisIndex" element={<AnalysisIndex />} />
-      <Route path="/PrimerTemp" element={<PrimerTemp />} />
-      <Route path="/Map" element={<CanadaMap />} />
+      <Route path="/analysis-index" element={<AnalysisIndex />} />
+      <Route path="/primer-temp" element={<PrimerTemp />} />
+      <Route path="/canada-map" element={<CanadaMap />} />
     </Routes>
   );
 }
--- src/pages/PrimerTemp.js
+++ src/pages/PrimerTemp.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import "./test.css"
 
 export function PrimerTemp() {
