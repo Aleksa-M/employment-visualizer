ee54c55ed1d00181705e74815c15fc71a5b917ef
Aleksa-M <a2misic@uwaterloo.ca>
2025-08-15 14:23:13 -0400

    added process.env things

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
+process.env["BACKEND_URL"] = "https://employment-visualizer.onrender.com";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
